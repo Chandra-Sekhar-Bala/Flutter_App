a6d1c815f1982e58ce583667b10617af115bfe07
Chandra-Sekhar-Bala <sekhar.chandra.5832@gmail.com>
2024-09-03 14:43:27 +0530

    impl: Animation on button press in stateful

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app_test/utils/routes.dart';
 
@@ -10,6 +12,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   var name = "";
+  var isButtonClicked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +39,8 @@ class _LoginPageState extends State<LoginPage> {
                   TextFormField(
                     decoration: const InputDecoration(
                         hintText: "Enter name", labelText: "Name"),
-                    onChanged: (value) {
+                    onChanged: (value) async {
+                      await(Future.delayed(const Duration(milliseconds: 200)));
                       name = value;
                       setState(() {});
                     },
@@ -51,21 +55,51 @@ class _LoginPageState extends State<LoginPage> {
                   const SizedBox(
                     height: 20,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  // Can Wrap around with Gesture detector(Do not give feedback on click)/ InkWell
+                  GestureDetector(
+                    onTap: () async {
+                      setState(() {
+                        isButtonClicked = !isButtonClicked;
+                      });
+                      await(Future.delayed(const Duration(seconds: 1)));
+                      Navigator.pushNamed(
+                          context, MyRoutes.homeRoute);
                     },
-                    style: TextButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blueAccent,
-                        minimumSize: const Size(double.infinity, 40)),
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 18,
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      width: isButtonClicked ? 60 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius:
+                        BorderRadius.circular(isButtonClicked ? 60 : 10),
+                        // shape: buttonClicked? BoxShape.circle: BoxShape.rectangle,
+                      ),
+                      child: isButtonClicked
+                          ? const Icon(Icons.done, color: Colors.white)
+                          : const Text(
+                        "Login",
+                        style:
+                        TextStyle(fontSize: 20, color: Colors.white),
                       ),
                     ),
-                  ),
+                  )
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  //   style: TextButton.styleFrom(
+                  //       foregroundColor: Colors.white,
+                  //       backgroundColor: Colors.blueAccent,
+                  //       minimumSize: const Size(double.infinity, 40)),
+                  //   child: const Text(
+                  //     "Login",
+                  //     style: TextStyle(
+                  //       fontSize: 18,
+                  //     ),
+                  //   ),
+                  // ),
                 ],
               ),
             )
